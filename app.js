95afb59adc79e4cd2ac7a41886605a7b38bb0e42
Lucas <lucas@agtlucas.com>
2014-11-30 00:20:55 -0200

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 var taunus = require('taunus');
 var taunusExpress = require('taunus-express');
 var express = require('express');
+var serverStatic = require('serve-static');
 var app = express();
 var options = {
   routes: require('./controllers/routes'),
   layout: require('./.bin/views/layout')
 };
 
+app.use(serverStatic('.bin/public'));
 taunusExpress(taunus, app, options);
 app.listen(3200);
