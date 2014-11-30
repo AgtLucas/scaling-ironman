6c3ce4435c5346d67c3db9020cb7a9c6c24e5b87
Lucas <lucas@agtlucas.com>
2014-11-29 23:30:26 -0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var taunus = require('taunus');
+var taunusExpress = require('taunus-express');
+var express = require('express');
+var app = express();
+var options = {};
+
+taunusExpress(taunus, app, options);
+app.listen(3000);
