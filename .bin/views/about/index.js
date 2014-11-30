d912689e5d8092bcaa5087970000c6f438135084
Lucas <lucas@agtlucas.com>
2014-11-30 10:12:47 -0200

    Compiled templates

--- .bin/views/about/index.js
+++ .bin/views/about/index.js
@@ -1,32 +1,32 @@
 var jade = require("jadum/runtime");
 module.exports = function index(locals) {
-var jade_debug = [{ lineno: 1, filename: "views/home/index.jade" }];
+var jade_debug = [{ lineno: 1, filename: "views/about/index.jade" }];
 try {
 var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-jade_debug.unshift({ lineno: 0, filename: "views/home/index.jade" });
-jade_debug.unshift({ lineno: 1, filename: "views/home/index.jade" });
+jade_debug.unshift({ lineno: 0, filename: "views/about/index.jade" });
+jade_debug.unshift({ lineno: 1, filename: "views/about/index.jade" });
 buf.push("<p>");
 jade_debug.unshift({ lineno: undefined, filename: jade_debug[0].filename });
 jade_debug.unshift({ lineno: 1, filename: jade_debug[0].filename });
-buf.push("Hello Taunus!");
+buf.push("About me section.");
 jade_debug.shift();
 jade_debug.shift();
 buf.push("</p>");
 jade_debug.shift();
-jade_debug.unshift({ lineno: 2, filename: "views/home/index.jade" });
-buf.push("<a href=\"/about\">");
+jade_debug.unshift({ lineno: 2, filename: "views/about/index.jade" });
+buf.push("<a href=\"/\">");
 jade_debug.unshift({ lineno: undefined, filename: jade_debug[0].filename });
 jade_debug.unshift({ lineno: 2, filename: jade_debug[0].filename });
-buf.push("About");
+buf.push("Go back");
 jade_debug.shift();
 jade_debug.shift();
 buf.push("</a>");
 jade_debug.shift();
 jade_debug.shift();;return buf.join("");
 } catch (err) {
-  jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "p Hello Taunus!\na(href=\"/about\") About");
+  jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "p About me section.\na(href=\"/\") Go back");
 }
 }
