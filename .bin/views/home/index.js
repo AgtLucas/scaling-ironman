31df21ac378bea02d3b9c9552087a904e564fcaf
Lucas <lucas@agtlucas.com>
2014-11-30 00:26:40 -0200

    Updates

--- .bin/views/home/index.js
+++ .bin/views/home/index.js
@@ -11,13 +11,13 @@ jade_debug.unshift({ lineno: 1, filename: "views/home/index.jade" });
 buf.push("<p>");
 jade_debug.unshift({ lineno: undefined, filename: jade_debug[0].filename });
 jade_debug.unshift({ lineno: 1, filename: jade_debug[0].filename });
-buf.push("Hello there!");
+buf.push("Hello Taunus!");
 jade_debug.shift();
 jade_debug.shift();
 buf.push("</p>");
 jade_debug.shift();
 jade_debug.shift();;return buf.join("");
 } catch (err) {
-  jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "p Hello there!");
+  jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "p Hello Taunus!");
 }
 }
